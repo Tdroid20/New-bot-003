389e03220cacc389fd4841878641153cf69f22c7
Tdroid2.0 <>
2021-04-17 02:42:20 +0000

    verificado 70%
    falta o bot mandar no canal especifico e mais umas coisinhas

--- src/commands/Owner bot/badges.js
+++ src/commands/Owner bot/badges.js
@@ -22,7 +22,7 @@ module.exports = class Badges extends Command {
   async run({ message, args, prefix, author }, t) {
       if(!owners.some(e => e == message.author.id)) return;
       const usuarioAlvo = this.client.users.cache.get(args[2]) || message.mentions.users.first() || message.author;
-
+			const deniedU = this.client.users.cache.get(args[1]) || message.mentions.users.first() || message.author;
       if(args[0] == "add") {
         User.findOne({ idU: usuarioAlvo.id }, async (e, res) => {
           if(!res.badges) res.badges = [];
@@ -32,7 +32,7 @@ module.exports = class Badges extends Command {
 				if(args[1] == "verify") {
 					
 //========================// mensagem no canal //========================//
-        	const canal = client.channels.cache.get('832439376205971476')
+        	const canal = '832439376205971476'
 					const chl = await new discord.MessageEmbed()
 						
 						.setColor('#35FF00')
@@ -49,7 +49,7 @@ module.exports = class Badges extends Command {
 						.setTimestamp()
 //enviando
 
-					 message.channel.send(chl)
+					client.channels.cache.get('832439376205971476').send(chl)
 //========================// mensagem na dm //========================//
 
          
@@ -100,6 +100,103 @@ module.exports = class Badges extends Command {
           await res.save();
         })
         message.channel.send("user edited")
-      }
+      } else if(args[0] == "approved") {
+        User.findOne({ idU: deniedU.id }, async (e, res) => {
+          if(!res.badges) res.badges = [];
+          res.badges.push("verify");
+          await res.save()
+        })
+
+				const approvedms = await new discord.MessageEmbed()
+					  .setAuthor(`aviso para ${deniedU.tag}`,deniedU.displayAvatarURL({ format: "jpg", size: 2048 }))	
+						.setTitle(`<:infoHf:832115931480195094> aviso!`)
+						.setColor('#35FF00')
+						.setDescription(`olá **${deniedU.tag}**! e com enorme prazer que vem dizer que **${message.author.tag}** aprovou seu pedido de se tornar um usuario verificado no bot(eu). agora voce tem em seu perfil e no userinfo do bot a insigna de usuario verificado, parabéns é muito difícil de obter essa insigna e esse titulo!`)
+						.addFields(
+							{
+								name: `<:staff:827404163126263808> enviada por`,
+								value: ` ${message.author.tag}`,
+								inline: true
+							},
+							{
+								name: `<:verifyHf:832451009849589800> Entregue para`,
+								value: `${deniedU.tag}`, inline: true
+							}
+						)
+						.addFields(
+							{
+								name: `bem como tudo tem um preço aqui tá`,
+								value: 'voce recebeu essa insigna mais precisa mostrar que a merece! voce precisa ajudar divugando o bot em suas redes sociais, com isso estará me ajuda a crescer e com isso dando oportunidade para mais gente adquirí-la'
+							}
+						)
+            .addFields(
+            	{
+            		name: `quer ver seu perfil?`,
+            		value: `[➕┃então me adicione a seu server](https://discord.com/oauth2/authorize?client_id=821548564421148692&scope=bot&permissions=2147483647)\nou pode
+[👑┃entrar no meu Servidor](https://discord.gg/NMv9a2dzHb)`
+            	}
+            	)
+            .setThumbnail(this.client.user.displayAvatarURL({ format: "jpg", size: 2048 }))
+						.setTimestamp();
+						
+						deniedU.send(approvedms)
+				
+			} else if (args[0] == "denied") {
+				const entregador = message.author.tag
+        const deniedms = await new discord.MessageEmbed()
+					  .setAuthor(`aviso para ${deniedU.tag}`,usuarioAlvo.displayAvatarURL({ format: "jpg", size: 2048 }))	
+						.setTitle(`<:infoHf:832115931480195094> aviso!`)
+						.setColor('#35FF00')
+						.setDescription(`olá **${deniedU}**! infelizmente venho dizer que seu pedido de verificação foi negado pelo **${entregador}**, desculpe dá essa notícia, mais voce pode tentar novamente ou pode perguntar o motivo da recusa para o ${entregador}`)
+						.addFields(
+							{
+								name: `<:staff:827404163126263808> negado por`,
+								value: ` ${message.author.tag}`,
+								inline: true
+							},
+							{
+								name: `<:verifyHf:832451009849589800> solicitado por`,
+								value: `${deniedU}`, inline: true
+							}
+						)
+						.addFields(
+							{
+								name: `oque acha de um segunda chanse`,
+								value: `[tente novamente](https://docs.google.com/forms/d/e/1FAIpQLSeCLg16GXNQRpK8hSZP_SuwYCtrurNOuAChQQXIxS12pGIq_g/viewform)`
+							}
+						)
+            .setThumbnail(this.client.user.displayAvatarURL({ format: "jpg", size: 2048 }))
+						.setTimestamp();
+						
+						deniedU.send(deniedms)
+      } else if (args[0] == "nr") {
+				
+        const nrms = await new discord.MessageEmbed()
+					  .setAuthor(`aviso para ${deniedU.tag}`,deniedU.displayAvatarURL({ format: "jpg", size: 2048 }))	
+						.setTitle(`<:infoHf:832115931480195094> aviso!`)
+						.setColor('#35FF00')
+						.setDescription(`olá **${deniedU.tag}**! infelizmente venho dizer que seu pedido de verificação foi negado pelo **${message.author.tag}** por não cumprir os requisitos`)
+						.addFields(
+							{
+								name: `<:staff:827404163126263808> negado por`,
+								value: ` ${message.author.tag}`,
+								inline: true
+							},
+							{
+								name: `<:verifyHf:832451009849589800> solicitado por`,
+								value: `${deniedU.tag}`, inline: true
+							}
+						)
+						.addFields(
+							{
+								name: `tente novamente apos cumprir os requisitos`,
+								value: `[enquanto aguarda entre em nosso servidor](https://discord.gg/NMv9a2dzHb)`
+							}
+						)
+            .setThumbnail(this.client.user.displayAvatarURL({ format: "jpg", size: 2048 }))
+						.setTimestamp();
+						
+						deniedU.send(nrms)
+      }	
   }
-}  
+}
